d66d24d8864e46e3219048f82b2bee1f679bf292
Kaiwen Sun <skw_kevin@126.com>
2016-06-05 16:00:11 -0700

    fix tiny bugs and security problems.

--- cmddict.py
+++ cmddict.py
@@ -27,6 +27,14 @@ errCodes = {
 	60:u"无词典结果，仅在获取词典结果生效"
 	}
 
+keylist = [
+		"keyfrom",
+		"key",
+		"type",
+		"doctype",
+		"version",
+		"q"
+		]
 
 def display(word,dic):
 	found = False
@@ -59,21 +67,32 @@ def lookup(word):
 	if not is_ascii(word):
 		print u"无法识别的英文"
 		return
+	if '&' in word and '=' in word and any(key in word for key in keylist):
+		print u"危险的查询"
+		return
 	reply = urllib2.urlopen("http://fanyi.youdao.com/openapi.do?keyfrom="+appname+"&key="+key+"&type=data&doctype=json&version=1.1&q="+word).read()
+	if type(reply) is str and reply=='no query':
+		print u"无效的输入"
+		return
 	dic = json.loads(reply)
 	display(word,dic)
 
 def main():
-	if len(sys.argv)>1:
-		word = " ".join(sys.argv[1:])
-		lookup(word)
-	else:
-		while True:
-		    try:
-				line = raw_input('> ')
-				lookup(line)
-		    except (EOFError,KeyboardInterrupt):
-		        break
+	try:
+		if len(sys.argv)>1:
+			word = " ".join(sys.argv[1:])
+			lookup(word)
+		else:
+			while True:
+			    try:
+					line = raw_input('> ')
+					if len(line)==0:
+						continue
+					lookup(line)
+			    except (EOFError,KeyboardInterrupt):
+			        break
+	except Exception as e:
+		print u"你好，程序测试员！"
 
 if __name__ == "__main__":
 	main()
